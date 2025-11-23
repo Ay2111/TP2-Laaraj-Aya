f7a1159124d02bf158dd49b889ad1602b77f1ede
Laaraj <a.laaraj01@gmail.com>
2025-11-23 19:34:27 +0100

    Add GitHub Actions workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// build.gradle.kts (Project: TP2AndroidApp)
 plugins {
     alias(libs.plugins.android.application) apply false
+    // Correction ici : on retire "jetbrains" car dans le TOML on l'a appelé "kotlin-android"
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
